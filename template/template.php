033b58d4c9f7fc311f0b57d44064a0ec83531e2a
--lasabahebwa <--lasabahebwa@zingsoft.com>
2022-08-05 01:36:57 +0300

    add zingchart

--- template/template.php
+++ template/template.php
@@ -1,31 +1,47 @@
 <!DOCTYPE html>
 <html>
+
 <head>
     <meta charset="utf-8" />
-    <meta http-equiv="x-ua-compatible" content="ie=edge"/>
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no"/>
+    <meta http-equiv="x-ua-compatible" content="ie=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
     <title><?php page_title(); ?> | <?php site_name(); ?></title>
-    <link href="<?php site_url(); ?>/template/style.css" rel="stylesheet" type="text/css" /> 
+    <link href="<?php site_url(); ?>/template/style.css" rel="stylesheet" type="text/css" />
+    <script src="https://cdn.zingchart.com/zingchart.min.js"></script>
 </head>
+
 <body>
-<div class="wrap">
+    <div class="wrap">
 
-    <header>
-        <h1><?php site_name(); ?></h1>
-        <nav class="menu">
-            <?php nav_menu(); ?>
-        </nav>
-    </header>
+        <header>
+            <h1><?php site_name(); ?></h1>
+            <nav class="menu">
+                <?php nav_menu(); ?>
+            </nav>
+        </header>
 
-    <article>
-        <h2><?php page_title(); ?></h2>
-        <?php page_content(); ?>
-    </article>
+        <article>
+            <h2><?php page_title(); ?></h2>
+            <?php page_content(); ?>
+            <div id="myChart"></div>
+        </article>
 
-    <footer>
-        <small>&copy;<?php echo date('Y'); ?> <?php site_name(); ?>.<br><?php site_version(); ?></small>
-    </footer>
+        <footer>
+            <small>&copy;<?php echo date('Y'); ?> <?php site_name(); ?>.<br><?php site_version(); ?></small>
+        </footer>
 
-</div>
+    </div>
+    <?php
+    include "vendor/autoload.php";
+
+    use ZingChart\PHPWrapper\ZC;
+
+    $zc = new ZC("myChart");
+    $zc->setChartType("area");
+    $zc->setSeriesData(0, [1, 4, 2, 6, 4]);
+    $zc->setTitle("My PHP graph title");
+    $zc->render();
+    ?>
 </body>
+
 </html>
---
+++ vendor/composer/installed.php
@@ -0,0 +1,56 @@
+<?php return array(
+    'root' => array(
+        'name' => '__root__',
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
+        'reference' => '543ef59be13ec6760cf94c96466928e40a1d76e8',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        '__root__' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'reference' => '543ef59be13ec6760cf94c96466928e40a1d76e8',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'monolog/monolog' => array(
+            'pretty_version' => '2.0.2',
+            'version' => '2.0.2.0',
+            'reference' => 'c861fcba2ca29404dc9e617eedd9eff4616986b8',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../monolog/monolog',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'psr/log' => array(
+            'pretty_version' => '1.1.4',
+            'version' => '1.1.4.0',
+            'reference' => 'd49695b909c3b7628b6289db5479a1c204601f11',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../psr/log',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'psr/log-implementation' => array(
+            'dev_requirement' => false,
+            'provided' => array(
+                0 => '1.0.0',
+            ),
+        ),
+        'zingchart/php_wrapper' => array(
+            'pretty_version' => '1.0.8',
+            'version' => '1.0.8.0',
+            'reference' => '576aa381599278ed40afd5489dd79fcc02aa0f28',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../zingchart/php_wrapper',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
